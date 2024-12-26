e35cc49c5b0892ec429aa9524bb99a42e72b6852
Lucas Fonseca Moreira <73448726+Spuntininki@users.noreply.github.com>
2024-12-26 23:27:52 +0000

    adicionado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Eae Carol, o lucas colocou um site no ar como teste, o que vc achou dele?</h1>
+	return (
+		<h1>
+			Eae Carol, o lucas colocou um site no ar como teste, o que vc achou
+			dele?
+		</h1>
+	);
 }
 
 export default Home;

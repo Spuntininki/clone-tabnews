52091350bab3df5a01792c04048df1f100cf43eb
Spuntininki <lucasfmoreira7@gmail.com>
2025-09-06 16:10:43 -0300

    adicionado try catch

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,38 +1,40 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 export default async function migrations(request, response) {
-	const allowedMethods = ["GET", "POST"];
-	if (!allowedMethods.includes(request.method)) {
-		return response.status(405).json(
-			{ error: `Method ${request.method} Not Allowed` }
-		);
+	try {
+		const allowedMethods = ["GET", "POST"];
+		if (!allowedMethods.includes(request.method)) {
+			return response.status(405).json(
+				{ error: `Method ${request.method} Not Allowed` }
+			);
+		}
+		
+
+		const defaultMigrationOptions = {
+			databaseUrl: process.env.DATABASE_URL,
+			dir: join("infra", "migrations"),
+			direction: "up",
+			verbose: true,
+			migrationsTable: "pgmigrations",
+		};
+
+		if (request.method === "GET") {
+			const pendingMigrations = await migrationRunner({
+				...defaultMigrationOptions,
+				dryRun: true,
+			});
+			return response.status(201).json(pendingMigrations);
+		}
+
+		if (request.method === "POST") {
+			const migratedMigrations = await migrationRunner({
+				...defaultMigrationOptions,
+				dryRun: false,
+			});
+			return response.status(200).json(migratedMigrations);
+		}
+	} catch (error) {
+		console.error(error);
+		return response.status(500).json({ error: "Internal Server Error" });
 	}
-	
-
-	const defaultMigrationOptions = {
-		databaseUrl: process.env.DATABASE_URL,
-		dir: join("infra", "migrations"),
-		direction: "up",
-		verbose: true,
-		migrationsTable: "pgmigrations",
-	};
-
-	if (request.method === "GET") {
-		const pendingMigrations = await migrationRunner({
-			...defaultMigrationOptions,
-			dryRun: true,
-		});
-		return response.status(201).json(pendingMigrations);
-	}
-
-	if (request.method === "POST") {
-		const migratedMigrations = await migrationRunner({
-			...defaultMigrationOptions,
-			dryRun: false,
-		});
-		return response.status(200).json(migratedMigrations);
-	}
-
-
-	return response.status(405).end();
 }
